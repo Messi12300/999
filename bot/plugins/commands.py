21b74a9673779019a6f23f6bb423c9c45e1ea6e2
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-10-01 15:59:03 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -135,7 +135,7 @@ async def start(bot, update):
         photo="https://telegra.ph/file/51b1eca426dab233a7796.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
-        reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup
     )
 
 
